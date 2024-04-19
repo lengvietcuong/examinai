a6e8ca2792701838d743831a2db5386e55c6d2f4
lengvietcuong <lengvietcuong@gmail.com>
2024-04-19 12:20:00 +0700

    update fonts

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Comfortaa } from 'next/font/google';
 import HeaderButtons from './HeaderButtons';
 import styles from './Header.module.css';
+
+const comfortaa = Comfortaa({subsets: ['latin'], weight: ['700']});
 
 const Header: React.FC = () => {
     return (
         <header className={styles.header}>
-            <h1 className={styles.heading}>Examin<span className={styles.accentText}>ai</span></h1>
+            <h1 className={`${styles.heading} ${comfortaa.className}`}>Examin<span className={styles.accentText}>ai</span></h1>
             <HeaderButtons />
         </header>
     );

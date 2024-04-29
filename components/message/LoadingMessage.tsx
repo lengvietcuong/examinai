4d301bed63da216e2a833d1c075461efd15dd257
lengvietcuong <lengvietcuong@gmail.com>
2024-04-29 21:21:35 +0700

    update loading message styles

--- components/message/LoadingMessage.tsx
+++ components/message/LoadingMessage.tsx
@@ -5,8 +5,7 @@ import styles from "./LoadingMessage.module.css";
 const LoadingMessage: React.FC = () => {
     return (
         <Message role="assistant">
-            <p>
-                Just a second
+            <p className={styles.loadingMessage}>
                 <span className={styles.dot}></span>
                 <span className={styles.dot}></span>
                 <span className={styles.dot}></span>

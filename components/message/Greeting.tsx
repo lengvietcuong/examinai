0d308bb867c7ff085881c00b4bcf7694221c2dc7
lengvietcuong <lengvietcuong@gmail.com>
2024-05-08 13:35:03 +0700

    increase greeting message generation speed

--- components/message/Greeting.tsx
+++ components/message/Greeting.tsx
@@ -6,8 +6,8 @@ import SkillSelection from "../SkillSelection";
 import useConversationStore from "@/stores/conversationStore";
 
 const Greeting: React.FC = () => {
-    const text = `Hi! I'm your IELTS examiner, here to help you prepare for your test.\nI'm an imperfect AI, so mistakes are possible.\n\nPlease select what you'd like to practice:`;
-    const delay = 7.5;
+    const text = `Hi! I'm your IELTS examiner, here to help you prepare for your test.\nI'm an imperfect AI, so mistakes are possible.\n\nWhat would you like to practice?`;
+    const delay = 3;
     const [currentText, setCurrentText] = useState('');
     const [currentIndex, setCurrentIndex] = useState(0);
     const isNewConversation = useConversationStore(state => state.isNewConversation);

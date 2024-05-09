ea07285a4d0afc90bd1e64729fd83ddb2ec19593
lengvietcuong <lengvietcuong@gmail.com>
2024-05-09 09:47:05 +0700

    add lazy loading to conversation history

--- components/sidebar/ConversationHistory.tsx
+++ components/sidebar/ConversationHistory.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '@/lib/firebase';
-import { collection, onSnapshot, query, DocumentData, doc, deleteDoc, orderBy } from 'firebase/firestore';
+import { collection, onSnapshot, query, DocumentData, doc, deleteDoc, orderBy, limit } from 'firebase/firestore';
 import useConversationStore from '@/stores/conversationStore';
 import useSkillStore from '@/stores/skillStore';
 import useUserMessageStore from '@/stores/userMessageStore';
@@ -11,43 +11,56 @@ import ConversationIcon from '../icons/ConversationIcon';
 import styles from './ConversationHistory.module.css';
 
 const ConversationHistory: React.FC = () => {
-    const { setMessages, conversationId, setConversationId, setIsNewConversation } = useConversationStore((state) => ({ setMessages: state.setMessages, conversationId: state.conversationId, setConversationId: state.setConversationId, setIsNewConversation: state.setIsNewConversation}));
+    const { setMessages, conversationId, setConversationId, setIsNewConversation } = useConversationStore((state) => ({ setMessages: state.setMessages, conversationId: state.conversationId, setConversationId: state.setConversationId, setIsNewConversation: state.setIsNewConversation }));
     const setSelectedSkill = useSkillStore((state) => state.setSelectedSkill);
     const setUserMessage = useUserMessageStore((state) => state.setUserMessage);
     const [user, loading] = useAuthState(auth);
-    const [conversations, setConversations] = useState<Record<string, DocumentData>>({});
+    const [conversations, setConversations] = useState<DocumentData[]>([]);
+    const [docLimit, setDocLimit] = useState(30);
+    const [hasMore, setHasMore] = useState(true);
+    const observer = useRef<IntersectionObserver | null>(null);
 
     useEffect(() => {
-        if (user) {
-            const fetchConversations = () => {
-                const conversationsRef = collection(db, `chats/${user.uid}/conversations`);
-                const q = query(conversationsRef, orderBy('lastModified', 'desc')); // Order by lastModified field in descending order
-                const unsubscribe = onSnapshot(q, (snapshot) => {
-                    setConversations(snapshot.docs.reduce((acc, doc) => ({ ...acc, [doc.id]: doc.data() }), {}));
-                });
+        if (!user) return;
+        let conversationsRef = collection(db, `chats/${user.uid}/conversations`);
+        let q = query(conversationsRef, orderBy('lastModified', 'desc'), limit(docLimit));
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            const newConversations = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setConversations(newConversations);
+            setHasMore(snapshot.docs.length === docLimit);
+        });
 
-                return () => unsubscribe();
-            };
-            fetchConversations();
-        }
-    }, [user]);
+        return () => unsubscribe();
+    }, [user, docLimit]);
+
+    const lastConversationElementRef = useCallback((node: HTMLLIElement | null) => {
+        if (observer.current) observer.current.disconnect();
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && hasMore) {
+                setDocLimit(prev => prev + 30);
+            }
+        });
+        if (node) observer.current.observe(node);
+    }, [hasMore]);
+
     const deleteConversation = async (id: string) => {
         if (!user) return;
 
         if (id === conversationId) {
-            // Delete current conversation
             setIsNewConversation(true);
             setConversationId(null);
             setSelectedSkill(null);
             setMessages(() => []);
             setUserMessage(null);
         }
+        setConversations(conversations.filter(conversation => conversation.id !== id));
+
         const conversationRef = doc(db, `chats/${user.uid}/conversations`, id);
         await deleteDoc(conversationRef);
     };
 
     const handleSelectConversation = async (id: string) => {
-        const conversation = conversations[id];
+        const conversation = conversations.find(conversation => conversation.id === id);
         if (!conversation) return;
         setIsNewConversation(false);
         setConversationId(id);
@@ -73,17 +86,21 @@ const ConversationHistory: React.FC = () => {
 
     return (
         <ul className={styles.conversationHistory}>
-            {Object.keys(conversations).map((id, i) => {
-                let conversation = conversations[id];
+            {conversations.map((conversation, i) => {
                 let skillCode = conversation.skill.charAt(0) + conversation.skill.slice(-1);
-                let isHighlighted = conversationId === id;
+                let isHighlighted = conversationId === conversation.id;
                 return (
-                    <li key={i} className={`${styles.conversation} ${isHighlighted ? styles.highlighted : ''}`} onClick={() => handleSelectConversation(id)}>
+                    <li
+                        key={i}
+                        ref={i === conversations.length - 1 ? lastConversationElementRef : null}
+                        className={`${styles.conversation} ${isHighlighted ? styles.highlighted : ''}`}
+                        onClick={() => handleSelectConversation(conversation.id)}
+                    >
                         <div className={styles.skillCodeContainer}>
                             <span className={styles.skillCode}>{skillCode}</span>
                         </div>
                         <span className={styles.conversationName}>{conversation.name}</span>
-                        <TrashcanIcon className={styles.trashcanIcon} onClick={(e) => { e.stopPropagation(); deleteConversation(id); }} />
+                        <TrashcanIcon className={styles.trashcanIcon} onClick={(e) => { e.stopPropagation(); deleteConversation(conversation.id); }} />
                     </li>
                 )
             })}

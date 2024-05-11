8c0b7ec6a6c3bf1d689f899b4ec53f6231d34d3e
lengvietcuong <lengvietcuong@gmail.com>
2024-05-11 22:38:15 +0700

    improve model output rendering

--- components/message/StylizedText.tsx
+++ components/message/StylizedText.tsx
@@ -1,32 +1,14 @@
 import React from "react";
+import ReactMarkdown from 'react-markdown';
 
 interface StylizedTextProps {
-  text: string;
+	text: string;
 }
 
 const StylizedText: React.FC<StylizedTextProps> = ({ text }) => {
-    const stylize = (text: string) => {
-        const regex = /(\*\*[^*]+\*\*)|(_[^_]+_)|([^\*_]+)/g;
-        let match;
-        let result = [];
-
-        while ((match = regex.exec(text)) !== null) {
-            let segment = match[0];
-            if (segment.startsWith('**') && segment.endsWith('**')) {
-                const displayText = segment.slice(2, -2);
-                result.push(<strong key={match.index}>{displayText}</strong>);
-            } else if (segment.startsWith('_') && segment.endsWith('_')) {
-                const cleanedSegment = segment.slice(1, -1);
-                result.push(<em key={match.index}>{cleanedSegment}</em>);
-            } else {
-                result.push(segment);
-            }
-        }
-
-        return result;
-    };
-
-    return <p>{stylize(text)}</p>;
+	return <div>
+		<ReactMarkdown>{text}</ReactMarkdown>
+	</div>
 };
 
 export default StylizedText;
